056b35661154b53491fde3ab3625bece2c47cd0a
Sheepyhead <trojes@tuta.io>
2022-03-08 19:52:21 +0100

    Initial die spawning

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,23 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugin(RapierPhysicsPlugin::<NoUserData>::default())
+        .add_startup_system(spawn_die)
         .run();
+}
+
+fn spawn_die(mut commands: Commands) {
+    commands
+        .spawn_bundle(ColliderBundle {
+            shape: ColliderShape::cuboid(0.05, 0.05, 0.05).into(),
+            ..ColliderBundle::default()
+        })
+        .insert_bundle(RigidBodyBundle {
+            ccd: RigidBodyCcd {
+                ccd_enabled: true,
+                ..RigidBodyCcd::default()
+            }
+            .into(),
+            ..RigidBodyBundle::default()
+        })
+        .insert(RigidBodyPositionSync);
 }
